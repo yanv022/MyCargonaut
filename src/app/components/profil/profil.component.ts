3f3633673f626651164b44d2b10817f9d5a04be5
winniecarole <winnie.carole.tongle.djoukeng@mni.thm.de>
2022-06-05 23:48:20 +0200

    add profil page design

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
+
+  defaultDate="1995-12-12";
+
+
   constructor() { }
 
   ngOnInit(): void {
