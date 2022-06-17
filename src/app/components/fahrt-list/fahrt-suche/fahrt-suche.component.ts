7a8b2de405ce010085b160e89e300139773532ac
winniecarole <winnie.carole.tongle.djoukeng@mni.thm.de>
2022-06-17 13:13:52 +0200

    add content zu Fahrtsuche

--- src/app/components/fahrt-list/fahrt-suche/fahrt-suche.component.ts
+++ src/app/components/fahrt-list/fahrt-suche/fahrt-suche.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import {CarsService} from "../../../services/cars.service";
 
 @Component({
   selector: 'app-fahrt-suche',
@@ -7,12 +8,30 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./fahrt-suche.component.scss']
 })
 export class FahrtSucheComponent implements OnInit {
+  @Input() name: any;
+  title!: string;
+  where!: string;
+  to!: string;
+  date!: any;
+  time!: any;
+  description!: string;
+  cars!: any[];
+  selectedCar!: any;
+  @Output() submit = new EventEmitter<{}>();
 
-  constructor(private activeModal: NgbActiveModal) {}
+  constructor(private activeModal: NgbActiveModal,private carsService: CarsService) {
+    this.cars = [{brand: "Ford", color: "blau", model:"Focus", seats:"5", storageInKg: "500", type:"combi", autoId: 'kRO6mCbnb5hScGXGThJt'},
+      {brand: "Opel", color: "grün", model:"Corsa", seats:"3", storageInKg: "200", type:"combi", autoId: 'kRO6mCbnb5hScGXGThJt'}
+    ]
+  }
   ngOnInit() {
+    this.carsService.getCarsOfUser('f29hRnOBQh1tb4AKZGev');
   }
   closeModal() {
     this.activeModal.close('Modal Closed');
+  }
+  setCar(car: any){
+    this.selectedCar = car;
   }
 
 }
