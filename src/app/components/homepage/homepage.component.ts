027cfc4ffe4358b37150331491c4c951d33efd72
Winnie Carole Tongle Djoukeng <winnie.carole.tongle.djoukeng@mni.thm.de>
2022-06-25 21:21:07 +0200

    home aktualisiert

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -11,14 +12,16 @@ export class HomepageComponent implements OnInit {
   showNavigationIndicators = false;
   images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  constructor(config: NgbCarouselConfig) {
+  constructor(config: NgbCarouselConfig, private route: ActivatedRoute,private router: Router ) {
     // customize default values of carousels used by this component tree
     config.showNavigationArrows = true;
     config.showNavigationIndicators = true;
   }
 
 
-
+  navigue(){
+    this.router.navigate([''])
+  }
 
   ngOnInit(): void {
   }
